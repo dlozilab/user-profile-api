ea9a1c9226ee1d278fac3415d4f91a5a742943c5
dlozilab <dlozi@mlab.co.za>
2024-11-04 11:02:04 +0200

    feat: added a GET root route / with a welcome message

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use('/users', userRoutes);
 // Error handling middleware
 app.use(errorHandler);
 
+app.get('/',(req,res)=>{
+    res.status(200).send("Welcome to User Profile MGMT");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
